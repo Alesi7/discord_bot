7a03ccc583363678c77b0c8181e6e72fcc817da3
Alexis <alexis.az2014@gmail.com>
2022-09-27 23:44:13 +0200

    commit 11

--- commands/gei.js
+++ commands/gei.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('qn es gei')
+		.setName('gei')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('el miguelter');
